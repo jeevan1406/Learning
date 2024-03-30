992e06068d38c540756b17e9644ff90aab652b9f
jeevan1406 <jeevan1406@gmail.com>
2024-03-30 07:05:28 +0000

    warning-aleart-added

--- Angular/pratice-app/src/app/app.module.ts
+++ Angular/pratice-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAleart } from './warning-aleart/warning-aleart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,WarningAleart
   ],
   imports: [
     BrowserModule
