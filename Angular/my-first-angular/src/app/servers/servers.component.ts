33008feca0e31c0ff6842e2fd5ad7e803c662e65
jeevan1406 <jeevan1406@gmail.com>
2024-03-30 15:07:19 +0000

    event creation

--- Angular/my-first-angular/src/app/servers/servers.component.ts
+++ Angular/my-first-angular/src/app/servers/servers.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowServer=false;
+  serverCreationStatus='No server Created!'
 
+  constructor(){
+    setTimeout(()=>{
+      this.allowServer=true;
+    },2000)
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onServerCreate(){
+    this.serverCreationStatus='Server Created!'
+  }
 }
