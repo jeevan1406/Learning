f19ff4d99c6036a6a34bc6f29aafe340353b02c4
jeevan1406 <jeevan1406@gmail.com>
2024-03-30 07:16:23 +0000

    success-aleart-added

--- Angular/pratice-app/src/app/app.module.ts
+++ Angular/pratice-app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WarningAleart } from './warning-aleart/warning-aleart.component';
+import { SuccessAleartComponent } from './success-aleart/success-aleart.component';
 
 @NgModule({
   declarations: [
-    AppComponent,WarningAleart
+    AppComponent, WarningAleart, SuccessAleartComponent
   ],
   imports: [
     BrowserModule
