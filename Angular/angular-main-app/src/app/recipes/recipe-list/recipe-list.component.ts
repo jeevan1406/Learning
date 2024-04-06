6be58dcf997786ad5583b70af548f51174996395
jeevan1406 <jeevan1406@gmail.com>
2024-04-06 14:47:37 +0100

    Recipe details updated

--- Angular/angular-main-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/angular-main-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../receipe.model';
+ 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] =[
+  new Recipe('A Test Recipe','Test recipe Description'
+  ,'https://images.unsplash.com/photo-1494390248081-4e521a5940db?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D'),
+  new Recipe('A Test Recipe','Test recipe Description'
+  ,'https://images.unsplash.com/photo-1494390248081-4e521a5940db?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D'),
+  new Recipe('A Test Recipe','Test recipe Description'
+  ,'https://images.unsplash.com/photo-1494390248081-4e521a5940db?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D'),
+  new Recipe('A Test Recipe','Test recipe Description'
+  ,'https://images.unsplash.com/photo-1494390248081-4e521a5940db?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlc3xlbnwwfHwwfHx8MA%3D%3D')
+];
 
+constructor(){}
+
+ngOnInit(): void {
+  
+}
 }
